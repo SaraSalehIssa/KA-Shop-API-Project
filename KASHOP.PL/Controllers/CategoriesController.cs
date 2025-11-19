a83a6978be10ed4809b0dfbf3c30141f92a1b935
SaraIssa <issasalehsara2001@gmail.com>
2025-11-19 23:18:14 +0200

    version 1

---
+++ KASHOP.PL/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using KASHOP.DAL.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KASHOP.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+    }
+}
---
+++ KASHOP.PL/Program.cs
@@ -0,0 +1,51 @@
+
+using KASHOP.DAL;
+using KASHOP.DAL.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace KASHOP.PL
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            //builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            //options.UseSqlServer(builder.Configuration["ConnectionStrings: DefaultConnection"]));
+
+            // OR
+
+            //builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            //options.UseSqlServer(builder.Configuration.GetSection("ConnectionStrings")["DefaultConnection"]));
+            
+            // OR
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
